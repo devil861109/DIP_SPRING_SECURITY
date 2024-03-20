04f9184c8c75e8e303b415aa08ef0e6e18aa45a0
devil861109 <devil861109@gmail.com>
2024-03-20 00:36:05 -0600

    Initial Application - Spring Security OAuth2 KeyCloak Integration

--- src/main/java/edu/unam/springsecurity/security/SecurityConfiguration.java
+++ src/main/java/edu/unam/springsecurity/security/SecurityConfiguration.java
@@ -1,73 +1,36 @@
 package edu.unam.springsecurity.security;
 
-import edu.unam.springsecurity.security.jwt.JWTAuthenticationFilter;
-import edu.unam.springsecurity.security.jwt.JWTTokenProvider;
-import edu.unam.springsecurity.security.jwt.JWTUsernameAndPasswordAuthenticationFilter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.security.SecureRandom;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
-    @Autowired
-    private UserDetailsService uds;
-    @Autowired
-    private JWTTokenProvider tokenProvider;
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeyCloakRoleConverter());
         http
                 .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("/user").hasAnyRole("USER")
-                        .requestMatchers("/admin").hasAnyRole("ADMIN")
-                        .requestMatchers("/api/**").permitAll()
-                        .requestMatchers("/v1/**").permitAll()
+                        //.requestMatchers("/v1/contact-type/**").hasAnyRole("TEST")
+                        .requestMatchers("/v1/contact-type/**").hasAnyRole("USER", "ADMIN")
                         .anyRequest().authenticated()
                 )
-                .addFilter(new JWTUsernameAndPasswordAuthenticationFilter())
-                .addFilterAfter(new JWTAuthenticationFilter(tokenProvider),JWTUsernameAndPasswordAuthenticationFilter.class)
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth2ResourceServerCustomizer ->
+                        oauth2ResourceServerCustomizer.jwt(jwtCustomizer -> jwtCustomizer
+                                .jwtAuthenticationConverter(jwtAuthenticationConverter)))
         ;
         return http.build();
-    }
-
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(11, new SecureRandom());
-    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(uds);
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-        return authenticationProvider;
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
-            throws Exception {
-        //your AuthenticationProvider must return UserDetails object
-        return new ProviderManager(authenticationProvider());
     }
 }
