fd1ea39f3cf6de2e5ba1904c2894fbd64a52a4b3
devil861109 <devil861109@gmail.com>
2024-03-19 23:15:49 -0600

    Initial Application - Spring Security LDAP Integration UnboundID

--- src/main/java/edu/unam/springsecurity/security/SecurityConfiguration.java
+++ src/main/java/edu/unam/springsecurity/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package edu.unam.springsecurity.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.ldap.core.LdapTemplate;
@@ -7,25 +8,25 @@ import org.springframework.ldap.core.support.BaseLdapPathContextSource;
 import org.springframework.ldap.core.support.LdapContextSource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.ldap.LdapBindAuthenticationManagerFactory;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
+//@EnableWebSecurity
 public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authz) -> authz
-                        .anyRequest().fullyAuthenticated())
+        http.authorizeHttpRequests((authorize) -> authorize.anyRequest().fullyAuthenticated())
                 .formLogin(Customizer.withDefaults())
         ;
         return http.build();
     }
 
-    @Bean
+    /*@Bean
     public LdapTemplate ldapTemplate() {
         return new LdapTemplate(contextSource());
     }
@@ -45,5 +46,19 @@ public class SecurityConfiguration {
         factory.setUserDnPatterns("cn={0},ou=users,ou=system");
         //factory.setUserDnPatterns("cn={0},ou=users");
         return factory.createAuthenticationManager();
+    }*/
+
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .ldapAuthentication()
+                .userDnPatterns("uid={0},ou=people")
+                .groupSearchBase("ou=groups")
+                .contextSource()
+                .url("ldap://localhost:8389/dc=springframework,dc=org")
+                .and()
+                .passwordCompare()
+                //.passwordEncoder(new BCryptPasswordEncoder())
+                .passwordAttribute("userPassword");
     }
 }
