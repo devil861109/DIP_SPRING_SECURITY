0d96f5359872cda6fcda6984246224f114406bf1
devil861109 <devil861109@gmail.com>
2024-03-19 22:17:17 -0600

    Initial Application - Spring Security User Role DB Creation

--- src/main/java/edu/unam/springsecurity/security/SecurityConfiguration.java
+++ src/main/java/edu/unam/springsecurity/security/SecurityConfiguration.java
@@ -1,9 +1,15 @@
 package edu.unam.springsecurity.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,9 +17,14 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.security.SecureRandom;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+    @Autowired
+    private UserDetailsService uds;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -24,57 +35,32 @@ public class SecurityConfiguration {
                         .anyRequest().authenticated()
                 )
                 .formLogin(login -> login
-                        .loginPage("/login") //new
-                        //.usernameParameter("email")
-                        //.passwordParameter("pass")
-                        //.loginProcessingUrl("/doLogin")
+                        .loginPage("/login")
                         .defaultSuccessUrl("/")
                         .successForwardUrl("/login_success_handler")
-                        //.failureForwardUrl("/login_failure_handler")
-                        /*.successHandler(new AuthenticationSuccessHandler() {
-                            @Override
-                            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                                System.out.println("Logged user: " + authentication.getName());
-                                response.sendRedirect("/");
-                            }
-                        })
-                        .failureHandler(new AuthenticationFailureHandler() {
-                            @Override
-                            public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-                                System.out.println("Login failed");
-                                System.out.println(exception);
-                                response.sendRedirect("/login");
-                            }
-                        })*/
                         .permitAll())
                 .logout(logout -> logout
                         .logoutUrl("/doLogout")
                         .logoutSuccessUrl("/")
-                        /*.logoutSuccessHandler(new LogoutSuccessHandler() {
-                            @Override
-                            public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
-                                    throws IOException, ServletException {
-                                System.out.println("This user logged out: " + authentication.getName());
-                                response.sendRedirect("/");
-                            }
-                        })*/
+                        .deleteCookies("JSESSIONID") //NEW Cookies to clear
                         .invalidateHttpSession(true))
-        //.csrf(csrf -> csrf.disable())
+                .csrf(Customizer.withDefaults())
+                .cors(Customizer.withDefaults())
+                .authenticationProvider(authenticationProvider())
         ;
         return http.build();
     }
 
     @Bean
     PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        //return new BCryptPasswordEncoder(11, new SecureRandom());
-        //return new BCryptPasswordEncoder(BCryptPasswordEncoder.BCryptVersion.$2Y, 12);
+        return new BCryptPasswordEncoder(11, new SecureRandom());
     }
 
     @Bean
-    UserDetailsManager inMemoryUserDetailsManager() {
-        var user1 = User.withUsername("user").password("{noop}password").roles("USER").build();
-        var user2 = User.withUsername("admin").password("{noop}password").roles("USER", "ADMIN").build();
-        return new InMemoryUserDetailsManager(user1, user2);
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(uds);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
     }
 }
--- src/test/java/edu/unam/springsecurity/auth/repository/UserInfoRepositoryTest.java
+++ src/test/java/edu/unam/springsecurity/auth/repository/UserInfoRepositoryTest.java
@@ -3,15 +3,21 @@ package edu.unam.springsecurity.auth.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import edu.unam.springsecurity.auth.model.UserInfo;
+import edu.unam.springsecurity.auth.model.UserInfoRole;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.annotation.Rollback;
 
+import java.security.SecureRandom;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
@@ -32,7 +38,7 @@ class UserInfoRepositoryTest {
         user.setUseFirstName("Alfonso");
         user.setUseLastName("Rivero");
         user.setUseEmail("devil861109@gmail.com");
-        user.setUsePasswd("asdf1234");
+        user.setUsePasswd("asdf1234"); //user
         user.setUseIdStatus(1);
         user.setUseCreatedBy(1L);
         user.setUseModifiedBy(1L);
@@ -52,6 +58,54 @@ class UserInfoRepositoryTest {
             assertThat(user.getUseEmail()).isEqualTo(existUser.getUseEmail());
         } else
             assertThat(user.getUseEmail()).isEqualTo(null);
+    }
 
+    @Test
+    public void testCreateUser2() {
+        UserInfo user = new UserInfo();
+        user.setUseFirstName("Gregorio");
+        user.setUseLastName("Duarte");
+        user.setUseEmail("devil861109@hotmail.com");
+        user.setUsePasswd("asdf1234"); //user
+        user.setUseIdStatus(1);
+        user.setUseCreatedBy(1L);
+        user.setUseModifiedBy(1L);
+        UserInfo savedUser = userInfoRepository.save(user);
+        UserInfo existUser = testEntityManager.find(UserInfo.class, savedUser.getUseId());
+        assertThat(user.getUseEmail()).isEqualTo(existUser.getUseEmail());
+    }
+
+    @Test
+    public void testRoleCreationUser() {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(11, new SecureRandom());
+        Set<UserInfoRole> roles = new HashSet<>();
+        UserInfoRole role = new UserInfoRole();
+        role.setUsrId(1L);
+        roles.add(role);
+        Optional<UserInfo> userInfo = userInfoRepository.findById(1L);
+        if (userInfo.isPresent()) {
+            UserInfo user = userInfo.get();
+            user.setUseInfoRoles(roles);
+            user.setUsePasswd(passwordEncoder.encode("user"));// user
+            userInfoRepository.save(user);
+            System.out.println("User updated with roles");
+        }
+    }
+
+    @Test
+    public void testRoleCreationUser2() {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(11, new SecureRandom());
+        Set <UserInfoRole> roles = new HashSet<>();
+        UserInfoRole role = new UserInfoRole();
+        role.setUsrId(2L);
+        roles.add(role);
+        Optional<UserInfo> userInfo = userInfoRepository.findById(2L);
+        if (userInfo.isPresent()) {
+            UserInfo user = userInfo.get();
+            user.setUseInfoRoles(roles);
+            user.setUsePasswd(passwordEncoder.encode("admin"));// admin
+            userInfoRepository.save(user);
+            System.out.println("User updated with roles");
+        }
     }
 }
